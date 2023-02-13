607076dcd8d2b852e6a12446466dbfecc16217ad
bobharterink <49344016+bobharterink@users.noreply.github.com>
2023-02-13 20:13:56 +0100

    img

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
-const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
-const text = intro.querySelector('.h1');
-//END SECTION
-const section = document.querySelector('.section');
-const end = document.querySelector('.h1');
+const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
 
-//SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
+const frameCount = 120;
+const currentFrame = index => (
+  `http://127.0.0.1:5500/videoimg/${index.toString().padStart(4, '0')}.jpg`
+)
 
-//SCENES
-const scene = new ScrollMagic.Scene({
-    duration: 3000,
-    triggerELement: intro,
-    triggerHook: 0,
-}).addIndicators()
-.setPin(intro)
-.addTo(controller);
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
 
-//VIDEO ANIMATION
-let scrollpos = 0;
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=1920;
+canvas.height=1080;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
 
-scene.on('update', e => {
-    scrollpos = e.scrollPos / 1000;
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
 });
 
-setInterval(() => {
-    video.currentTime = scrollpos;
-}, 33.3);
-
-
-
-
-
+preloadImages()
