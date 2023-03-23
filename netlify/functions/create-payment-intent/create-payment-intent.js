478a108112b9dc8bbd2699ef6e5ad99da281d1b3
bobharterink <49344016+bobharterink@users.noreply.github.com>
2023-03-23 10:46:17 +0100

    Test payment intent

--- netlify/functions/create-payment-intent/create-payment-intent.js
+++ netlify/functions/create-payment-intent/create-payment-intent.js
@@ -1,13 +1,23 @@
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
+
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    // const subject = event.queryStringParameters.name || 'World'
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 30,
+      currency: 'eur',
+    });
+    
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret, amount: paymentIntent.amount }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
