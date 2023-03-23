457ef35eaa17a30b565abd8c0a75b969d8ea092c
Frederik De Bleser <frederik@debleser.be>
2023-03-23 11:08:08 +0100

    Make moar money

--- netlify/functions/create-payment-intent/create-payment-intent.js
+++ netlify/functions/create-payment-intent/create-payment-intent.js
@@ -2,7 +2,7 @@ const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-const stripe = require('stripe')(STRIPE_SECRET_KEY);
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
@@ -10,18 +10,20 @@ const handler = async (event) => {
     // const subject = event.queryStringParameters.name || 'World'
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 30,
-      currency: 'eur',
+      amount: 3000,
+      currency: "eur",
     });
-    
-    
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ clientSecret: paymentIntent.client_secret, amount: paymentIntent.amount }),
-    }
+      body: JSON.stringify({
+        clientSecret: paymentIntent.client_secret,
+        amount: paymentIntent.amount,
+      }),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
